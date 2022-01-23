d2fb677d1dbdb9b317833e37e4e6b48c34a1cbdf
Keith Hammond <keithammond@gmail.com>
2022-01-23 19:05:46 +1300

    changed source headers to reflect new license

--- astlib/clang.hpp
+++ astlib/clang.hpp
@@ -1,16 +1,10 @@
 /*
- *	Meta C++ Tool and Library
- *	Copyright (C) 2022  Keith Hammond
+ * Meta C++ Tool and Library
+ * Copyright (C) 2022  Keith Hammond
  *
- *	This library is free software; you can redistribute it and/or
- *	modify it under the terms of the GNU Lesser General Public
- *	License as published by the Free Software Foundation; either
- *	version 2.1 of the License, or (at your option) any later version.
- *
- *	This library is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *	Lesser General Public License for more details.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
 #ifndef METACPP_AST_CLANG_HPP
